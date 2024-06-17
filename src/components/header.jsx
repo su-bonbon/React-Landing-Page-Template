7925db5f8f09434cc4e47326aaf3b798b62e7899
Sujin <sujinbonnaud@gmail.com>
2024-06-17 10:39:16 -0500

    edd css on the main btn

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,18 +14,21 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <div className="container-btn">
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "}
+                  <a
+                    href="#features"
+                    className="btn btn-custom btn-lg page-scroll"
+                  >
+                    Learn More
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+</div>
+
+    
+
     </header>
   );
 };
